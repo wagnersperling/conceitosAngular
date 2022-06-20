832bd4a701f354542401d497e48ce089e132ef31
Carlos Wagner <carlos.sperling@granter.com.br>
2022-06-20 08:02:15 -0300

    FormBuilder API.

--- Forms/src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ Forms/src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,16 +9,24 @@ export class CadastroComponent implements OnInit {
 
 cadastroForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.cadastroForm = new FormGroup({
-      nome: new FormControl(''),
-      cpf: new FormControl(''),
-      email: new FormControl(''),
-      senha: new FormControl(''),
-      senhaConfirmacao: new FormControl('')
+    this.cadastroForm = this.fb.group({
+      nome: [''],
+      cpf: [''],
+      email: [''],
+      senha: [''],
+      senhaConfirmacao: ['']
     });
+
+    // this.cadastroForm = new FormGroup({
+    //   nome: new FormControl(''),
+    //   cpf: new FormControl(''),
+    //   email: new FormControl(''),
+    //   senha: new FormControl(''),
+    //   senhaConfirmacao: new FormControl('')
+    // });
   }
 
   adicionarUsuario(){
