c1fd3000cef03c2209a6a87e1a88ee4a984bdbda
Carlos Wagner <carlos.sperling@granter.com.br>
2022-07-08 07:32:07 -0300

    Tratamento de erro 404.

--- ProjetoModularizado/src/app/app.routes.ts
+++ ProjetoModularizado/src/app/app.routes.ts
@@ -3,15 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './navegacao/home/home.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
+import { NotFoundComponent } from './navegacao/not-found/not-found.component';
 
 const rootRouterConfig: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: '', redirectTo: '/home', pathMatch: 'full'},    
     { path: 'home', component: HomeComponent},
     { path: 'sobre', component: SobreComponent },
     { path: 'cadastro', component: CadastroComponent },
     { path: 'produtos', 
             loadChildren: () => import('./demos/arquitetura-componentes/produto.module')
-            .then(m => m.ProdutoModule)}
+            .then(m => m.ProdutoModule)},
+            
+    {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
