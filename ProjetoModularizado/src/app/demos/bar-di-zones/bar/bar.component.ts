7d4932e45d0a8d110649f50029617afe7435036e
Carlos Wagner <carlos.sperling@granter.com.br>
2022-08-01 17:13:15 -0300

    NgZones.

--- ProjetoModularizado/src/app/demos/bar-di-zones/bar/bar.component.ts
+++ ProjetoModularizado/src/app/demos/bar-di-zones/bar/bar.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { BarUnidadeConfig, BAR_UNIDADE_CONFIG } from './bar.config';
 import { BarFactory, BarService, BarServiceMock, BebidaService } from './bar.service';
-import { Component, Inject, Injector, OnInit } from '@angular/core';
+import { Component, Inject, Injector, NgZone, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bar',
@@ -32,7 +32,8 @@ export class BarComponent implements OnInit {
     private barService: BarService,
     @Inject('ConfigManualUnidade') private ApiConfigManual: BarUnidadeConfig,
     @Inject(BAR_UNIDADE_CONFIG) private ApiConfig: BarUnidadeConfig,
-    private bebidaService : BebidaService
+    private bebidaService : BebidaService,
+    private ngZone: NgZone
     ) { }
 
   ngOnInit() {
@@ -42,6 +43,37 @@ export class BarComponent implements OnInit {
     this.dadosUnidade =  this.barService.obterUnidade();
 
     this.barBebida2 = this.bebidaService.obterBebidas();
+  }
+
+  public progress: number = 0;
+  public label: string;
+
+  processWithinAngularZone(){
+    this.label = 'dentro';
+    this.progress = 0;
+    this._increaseProgress(() => console.log('Finaliado por dentro do Angular!'));
+  }
+
+  processOutsideOfAngularZone(){
+    this.label = 'fora';
+    this.progress = 0;
+    this.ngZone.runOutsideAngular(() => {
+      this._increaseProgress(() => {
+        this.ngZone.run(() => { console.log('Finalizado fora'); });
+      })
+    })
+  }
+
+
+  _increaseProgress(doneCallback: () => void){
+    this.progress += 1;
+    console.log(`Progresso atual ${this.progress}%`)
+
+    if(this.progress < 100){
+      window.setTimeout(() => this._increaseProgress(doneCallback), 10);
+    }else{
+      doneCallback();
+    }
   }
 
 }
