fc4809470e88da789d83be62fa1fed65fe72d0e2
Carlos Wagner <carlos.sperling@granter.com.br>
2022-08-25 11:09:06 -0300

    Gerenciando eventos de mudança de estado.

--- ProjetoModularizado/src/app/demos/todo-list/todo.service.ts
+++ ProjetoModularizado/src/app/demos/todo-list/todo.service.ts
@@ -20,4 +20,20 @@ export class TasksService{
     //     .get<Task[]>('http://localhost:3000/todolist');
     // }
 
+
+    toggle(event: any){
+        this.http
+        .put(`http://localhost:3000/todolist/${event.task.id}`, event.task)
+        .subscribe(()=> {
+            const value = this.store.value.todolist;
+            const todolist = value.map((task: Task) => {
+                if(event.task.id === task.id){
+                    return { ...task, ...event.task}
+                }else{
+                    return task;
+                }
+            })
+            this.store.set('todolist', todolist);
+        });
+    }
 }
